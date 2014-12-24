612b17c925cf850720d230d841180253a2474a02
ivan-yin <020lilin@gmail.com>
2014-12-24 18:58:19 +0800

    delete files not used

--- mybatis-generator/createTable.sql
+++ mybatis-generator/createTable.sql
@@ -1,6 +1,25 @@
+--oracle
 create table muser(
 id varchar2(36) primary key,
 name varchar2(36),
 age number(8),
 address varchar2(36)
 );
+
+
+--mysql
+
+-- ----------------------------
+--  Table structure for `muser`
+-- ----------------------------
+DROP TABLE IF EXISTS `muser`;
+CREATE TABLE `muser` (
+  `id` varchar(36) NOT NULL DEFAULT '',
+  `name` varchar(36) DEFAULT NULL,
+  `age` decimal(8,0) DEFAULT NULL,
+  `address` varchar(36) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+SET FOREIGN_KEY_CHECKS = 1;
